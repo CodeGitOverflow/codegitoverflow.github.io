f5091082e4e587ba4685911eee4261b774226110
CodeGitOverflow <151279532+CodeGitOverflow@users.noreply.github.com>
2023-11-19 14:15:52 -0500

    Update and rename question.json to question.js

---
+++ question.js
@@ -0,0 +1,5 @@
+const data = {
+  "title": document.getElementById("question-title").value,
+  "content": document.getElementById("question-content").value,
+  "owner": document.getElementById("question-owner").value,
+};
