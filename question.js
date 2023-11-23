57313569ce68f4522dff954974d388c735564bd0
CodeGitOverflow <151279532+CodeGitOverflow@users.noreply.github.com>
2023-11-23 15:17:07 -0500

    Delete question.js

--- question.js
+++
@@ -1,5 +0,0 @@
-const data = {
-  "title": document.getElementById("question-title").value,
-  "content": document.getElementById("question-content").value,
-  "owner": document.getElementById("question-owner").value,
-};
